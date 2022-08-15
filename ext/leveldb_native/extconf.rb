c52a50e8430897565d3e29b7d034d3c0140d002f
Benjamin ter Kuile <bterkuile@gmail.com>
2022-08-15 13:40:35 -0500

    Add compiler flag for darwin systems that does not compile

--- ext/leveldb_native/extconf.rb
+++ ext/leveldb_native/extconf.rb
@@ -4,4 +4,6 @@ dir_config('leveldb')
 
 have_library "leveldb" or abort "Can't find leveldb library."
 
+$CXXFLAGS += " -std=c++11 " if RUBY_PLATFORM =~ /darwin/
+
 create_makefile "leveldb-native/leveldb_native"
